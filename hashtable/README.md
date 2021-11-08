3b943719747d30766ca6f9eafe22a773ca80c3db
deyaa-pozan <deyaa.pozan@gmail.com>
2021-11-08 22:42:50 +0200

    challenge 30

---
+++ hashtable/README.md
@@ -0,0 +1,58 @@
+Implement a hashtable to store (key, value) pairs and retrieve the value in a fast way.
+
+## Challenge
+
+write the Java code to implement the hashtable and all its methods( add, get, contains, hash)
+to store and retrieve data from this data structure in time complexity `O(1)`
+where its method must do the following:
+
+* `add`: takes in both the key and value. This method should hash the key, and add the key and value pair to the table, handling collisions as needed.
+* `get`: takes in the key and returns the value from the table.
+* `contains`: takes in the key and returns a boolean, indicating if the key exists in the table already.
+* `hash`: takes in an arbitrary key and returns an index in the collection
+
+## Approach & Efficiency
+
+* ***Approach:***
+
+Create an array of linked lists.. (buckets)of size `99` to hold the table in it
+
+`hash(key):`
+
+take a key as argument
+calculate the sum for the ASCII code of each character in the key 
+multibly the sum with `599`, where 599 is a prime number.
+take the modulu of the result on the size of buckets:  `( result % 99 )`
+return the final result ( index )
+
+`add(key,value):`
+
+hash the key.. using `hash` method 
+go to the index returned from hash method in the buckets array.
+check if the key is exist in the linked list which is inside the current bucket
+if exist.. update the value of this key
+if not .. add the pair(key, value) to the linked list
+
+`get(key):`
+hash the key.. using hash method 
+go to the index returned from hash method
+if the key is exist in this bucket.. return its value.
+if not .. return null.
+
+`contains(key):`
+hash the key.. using hash method 
+go to the index returned from hash method
+if the key is exist in this bucket.. return true otherwise.. return false.
+
+* ***Efficiency:***
+
+***space: O(N)***
+
+where n is the size of the array of buckets.
+
+***time:*** 
+
+* for add, get, contains:
+  * average time : `O(1)`
+  * worst case : `O(n)` "when the all keys is hashed to the same index."
+* for hash: `O(1)`
---
+++ hashtable/app/src/main/java/hashtable/HashTable.java
@@ -0,0 +1,153 @@
+package hashtable;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class HashTable<K, V> {
+
+    // bucket of hashnodes used to store linked list of
+    // hashnodes in each bucket when collisions occur
+    private ArrayList<HashNode<K, V>> bucketArray;
+
+    // will hold the current size of our bucketArray
+    private int numBuckets;
+
+    // will hold the total number of hashnodes in the bucket array
+    private int size;
+
+    public HashTable() {
+        bucketArray = new ArrayList<>();
+        numBuckets = 10;
+        size = 0;
+
+        // create bucket array AKA simulate an actual array
+        for (int index = 0; index < numBuckets; index++) {
+            bucketArray.add(null);
+        }
+    }
+
+    private int hashCode(K key) {
+        return Objects.hashCode(key);
+    }
+
+    public int hash(K key) {
+        return Objects.hashCode(key)%numBuckets * -1;
+    }
+    
+
+    private int getBucketIndex(K key) {
+        int hashCode = hashCode(key);
+        int index = hashCode % numBuckets;
+        // hashCode can be negative (key.hashCode() can be -ve (negative))
+        // so we need to make it positive
+        index = index < 0 ? index * -1 : index;
+
+        return index;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return getSize() == 0;
+    }
+    public void add(K key, V value) {
+
+        // gets the head of a linked list for a given key
+        int bucketIndex = getBucketIndex(key);
+        int hashCode = hashCode(key);
+
+        HashNode<K, V> head = bucketArray.get(bucketIndex);
+
+        // check if key present
+        while (head != null) {
+            if (head.key.equals(key) && head.hashCode == hashCode) { // checks for duplicates
+                // if there is a duplicate just overwrite
+                head.value = value;
+                return;
+            }
+
+            // adds to the chain of the linkedlist, by adding to the front
+            head = head.next;
+        }
+
+        // insert the value in to the position
+        size++;
+        head = bucketArray.get(bucketIndex);
+        HashNode<K, V> newNode = new HashNode<>(key, value, hashCode);
+        newNode.next = head;
+        bucketArray.set(bucketIndex, newNode);
+
+        // if the load factor (number of hasnnodes) goes beyond the threshold
+        // double the hashtble aka the bucket array
+        if ((1.0 * size) / numBuckets >= 0.7) {
+            ArrayList<HashNode<K, V>> temp = bucketArray;
+            bucketArray = new ArrayList<>();
+            numBuckets = 2 * numBuckets;
+            size = 0;
+
+            // increase bucket array size
+            for (int index = 0; index < numBuckets; index++) {
+                bucketArray.add(null);
+            }
+
+            // copy back in the original data to the increased bucket array
+            for (HashNode<K, V> headNode : temp) {
+                while (headNode != null) {
+                    add(headNode.key, headNode.value);
+                    headNode = headNode.next;
+                }
+            }
+        }
+    }
+
+  
+
+    public V get(K key) {
+        int bucketIndex = getBucketIndex(key);
+        int hashCode = hashCode(key);
+        HashNode<K, V> head = bucketArray.get(bucketIndex);
+
+        while (head != null) {
+            if (head.key.equals(key) && head.hashCode == hashCode) {
+                return head.value;
+            }
+
+            head = head.next;
+        }
+
+        return null;
+    }
+
+    public boolean contains(K key){
+        int bucketIndex = getBucketIndex(key);
+        int hashCode = hashCode(key);
+        HashNode<K, V> head = bucketArray.get(bucketIndex);
+        while (head != null) {
+            if (head.key.equals(key) && hashCode == head.hashCode)
+                return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        if(this.size ==0)
+            return "Null";
+
+        StringBuilder out = new StringBuilder();
+        for (int i = 0 ; i < bucketArray.size(); i++) {
+            if (bucketArray.get(i)==null)
+                continue;
+            out.append(i);
+            out.append(" : [ ");
+            out.append(bucketArray.get(i).toString());
+            out.append(" ]\n");
+        }
+
+        return out.toString();
+    }
+
+}
